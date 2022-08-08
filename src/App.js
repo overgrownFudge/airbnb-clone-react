5a272ba2f756d0b00c2cf86f30984015d2673fa9
overgrownFudge <overgrownfudge@gmail.com>
2022-08-08 16:04:51 -0400

    imported css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
 import data from "./data"
+import "./style.css"
 
 export default function App() {
     const cards = data.map(item => {
